89f6becbb4ec4f22d92b7f2022ffc31580b58443
Yaxiri <yaxiri191@gmail.com>
2017-03-09 21:05:04 -0600

    TEXTSPEECH

--- app.js
+++ app.js
@@ -1,28 +1,50 @@
-/*eslint-env node*/
+/**
+ * Copyright 2015 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
+const express = require('express');
+const app = express();
+const watson = require('watson-developer-cloud');
 
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
+// Bootstrap application settings
+require('./config/express')(app);
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
-
-// create a new express server
-var app = express();
-
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
-
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+const stt = new watson.SpeechToTextV1({
+  // if left undefined, username and password to fall back to the SPEECH_TO_TEXT_USERNAME and
+  // SPEECH_TO_TEXT_PASSWORD environment properties, and then to VCAP_SERVICES (on Bluemix)
+  // username: '',
+  // password: ''
 });
+
+const authService = new watson.AuthorizationV1(stt.getCredentials());
+
+app.get('/', function(req, res) {
+  res.render('index', {
+    BLUEMIX_ANALYTICS: process.env.BLUEMIX_ANALYTICS,
+  });
+});
+
+// Get token using your credentials
+app.get('/api/token', function(req, res, next) {
+  authService.getToken(function(err, token) {
+    if (err) {
+      next(err);
+    } else {
+      res.send(token);
+    }
+  });
+});
+
+module.exports = app;
